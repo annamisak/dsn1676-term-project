4a3d61fed61444d63918ba4fd0e15ecb62e20cda
Anna <holdikana@gmail.com>
2015-04-20 12:51:43 -0400

    Code clean up

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ var $tabs = $('.tabs');
 var $panels = $('.panel');
 
 $tabs.on('click', 'a', function (e) {
-  e.preventDefault();
-  var id = $(this).attr('href');
+    e.preventDefault();
+    var id = $(this).attr('href');
 
-  $panels.filter(':not([hidden])').attr('hidden', true);
-  $(id).removeAttr('hidden');
+    $panels.filter(':not([hidden])').attr('hidden', true);
+    $(id).removeAttr('hidden');
 
-  $tabs.find('.js-current').removeClass('js-current');
-  $(this).addClass('js-current');
+    $tabs.find('.js-current').removeClass('js-current');
+    $(this).addClass('js-current');
 });
 
 $('.nav').localScroll();
@@ -23,14 +23,6 @@ $('.social-media').localScroll();
 var $bearbody = $('.bearbody');
 
 $bearbody.waypoint(function () {
-    $bearbody.addClass('js-active');
-    
-    TweenMax.to('.leg-left', 1, {rotation:25, transformOrigin:'50% 0', ease:Linear.easeInOut, repeat:-1});
-    
-    TweenMax.to('.leg-right', 1, {rotation:18, transformOrigin:'50% 0', ease:Linear.easeInOut, repeat:-1});
-
-    TweenMax.to('.arm-right', 1, {rotation:-60, transformOrigin:'0 50%', ease:Linear.easeInOut, repeat:-1});
-
-    TweenMax.to('.arm-left', 1, {rotation:-40, transformOrigin:'0 50%', ease:Linear.easeInOut, repeat:-1});
-    
-}, {offset: '40%'});
+    $bearbody.addClass('js-active');}, {
+    offset: '40%'
+});
